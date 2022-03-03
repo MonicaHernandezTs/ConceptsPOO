ea60299c28dafb9c77a37af6561793b56e2c26d5
MonicaLizeth <MonicaLizeth@DESKTOP-KS00F9U>
2022-03-02 22:07:22 -0500

    Finish date class

---
+++ POOConcepts/POOConcepts/NotImplementeException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConceptsPOO
+{
+    [Serializable]
+    internal class NotImplementeException : Exception
+    {
+        public NotImplementeException()
+        {
+        }
+
+        public NotImplementeException(string? message) : base(message)
+        {
+        }
+
+        public NotImplementeException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotImplementeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using ConceptsPOO;
+
+Console.WriteLine("ConceptsPOO");
+Console.WriteLine("============");
+
+try
+{
+    Console.WriteLine(new Date(2022, 12, 7));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 18, 45));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
+
